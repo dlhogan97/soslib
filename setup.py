97aae2a12dc4b4e7bc2edf68feb99a4610b88014
Danny Hogan <dlhogan@uw.edu>
2022-07-25 10:05:40 -0700

    updated and added a new package

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import find_packages, setup
 setup(
     name='soslib',
     packages=find_packages(include=['soslib']),
-    version='0.1.0',
+    version='0.2.0',
     description='Library for use during and after the sublimation of snow campaign',
     author='Daniel Hogan',
     license='MIT',
     install_requires=['xarray','nctoolkit','numpy','pandas'],
+    url='https://github.com/dlhogan97/soslib',
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
