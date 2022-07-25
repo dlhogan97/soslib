a9f416a2983087033038f697e9cd5071197c374f
Danny Hogan <dlhogan@uw.edu>
2022-07-25 10:33:09 -0700

    saved new version of package

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,4 +1,8 @@
 from soslib import funcs
+import datetime as dt
 
 def test_radsys():
     assert len(funcs.get_daily_radsys_data('2022-01-13', '2022-01-17'))==47
+
+def test_awdb():
+    funcs.get_awdb_data('site_ids', element="WTEQ", sdate=dt.datetime(1899,10,1), edate=dt.datetime.now(), orient="records", server="https://api.snowdata.info/", sesh=None)
